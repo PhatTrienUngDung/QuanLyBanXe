fa01aa3d1efa3890d7839326de80512eca6bc8ea
NguyenThanhHoai15 <73808149+NguyenThanhHoai15@users.noreply.github.com>
2020-11-06 19:54:56 +0700

    create Xe entity

--- src/entity/ChiTietHoaDon.java
+++ src/entity/ChiTietHoaDon.java
@@ -2,4 +2,10 @@ package entity;
 
 public class ChiTietHoaDon {
 
+	private HoaDon hoaDon;
+	private Xe sanPham;
+	private int soLuong;
+	private double donGia;
+	
+	
 }
--- src/entity/HoaDon.java
+++ src/entity/HoaDon.java
@@ -8,4 +8,67 @@ public class HoaDon {
 	private KhachHang khachHang;
 	private NhanVien nhanVien;
 	private LocalDate ngayLapHoaDon;
+	
+	public HoaDon() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public HoaDon(String maHoaDon) {
+		super();
+		this.maHoaDon = maHoaDon;
+	}
+
+	public HoaDon(String maHoaDon, KhachHang khachHang, NhanVien nhanVien, LocalDate ngayLapHoaDon) {
+		super();
+		this.maHoaDon = maHoaDon;
+		this.khachHang = khachHang;
+		this.nhanVien = nhanVien;
+		this.ngayLapHoaDon = ngayLapHoaDon;
+	}
+
+	public String getMaHoaDon() {
+		return maHoaDon;
+	}
+
+	public void setMaHoaDon(String maHoaDon) {
+		this.maHoaDon = maHoaDon;
+	}
+
+	public KhachHang getKhachHang() {
+		return khachHang;
+	}
+
+	public void setKhachHang(KhachHang khachHang) {
+		this.khachHang = khachHang;
+	}
+
+	public NhanVien getNhanVien() {
+		return nhanVien;
+	}
+
+	public void setNhanVien(NhanVien nhanVien) {
+		this.nhanVien = nhanVien;
+	}
+
+	public LocalDate getNgayLapHoaDon() {
+		return ngayLapHoaDon;
+	}
+
+	public void setNgayLapHoaDon(LocalDate ngayLapHoaDon) {
+		this.ngayLapHoaDon = ngayLapHoaDon;
+	}
+	
+	public double getThanhTien() {
+		
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return "HoaDon [maHoaDon=" + maHoaDon + ", khachHang=" + khachHang + ", nhanVien=" + nhanVien
+				+ ", ngayLapHoaDon=" + ngayLapHoaDon + "]";
+	}
+	
+	
 }
