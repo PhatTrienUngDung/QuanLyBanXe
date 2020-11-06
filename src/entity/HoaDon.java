a2021d5414fb084bd3b0d56cf73b66e4b904fe0a
NguyenThanhHoai15 <73808149+NguyenThanhHoai15@users.noreply.github.com>
2020-11-06 19:46:01 +0700

    create invoice, invoice details and account entity

---
+++ src/entity/HoaDon.java
@@ -0,0 +1,11 @@
+package entity;
+
+import java.time.LocalDate;
+
+public class HoaDon {
+
+	private String maHoaDon;
+	private KhachHang khachHang;
+	private NhanVien nhanVien;
+	private LocalDate ngayLapHoaDon;
+}
