6a82779247ee2d4a1d9bc984124f2a78f23f7b21
NguyenThanhHoai15 <73808149+NguyenThanhHoai15@users.noreply.github.com>
2020-11-19 09:35:15 +0700

    autoComplete

---
+++ src/autoComplete/FillCombo.java
@@ -0,0 +1,29 @@
+package autoComplete;
+
+import connect.ConnectDB;
+
+import java.sql.SQLException;
+
+import javax.swing.JComboBox;
+
+public class FillCombo {
+
+	ConnectDB con = new ConnectDB();
+	
+	public void fill(String SQL, JComboBox combo, String column) throws SQLException {
+        con.connect();
+        con.executaSQL(SQL);
+        try {
+            combo.removeAllItems();
+            con.rs.first();
+            do {
+                new CustomCombo(combo).custom(combo, con.rs.getString(column));
+                combo.setSelectedItem(null);
+            } while (con.rs.next());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //con.disconnect();
+        }
+    }
+}
