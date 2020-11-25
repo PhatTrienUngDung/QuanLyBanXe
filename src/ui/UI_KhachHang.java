351a4ce977d950bd9f0329f4fe626e7d516f1e0c
tuankhang39 <68111903+tuankhang39@users.noreply.github.com>
2020-11-25 09:59:39 +0700

    Khach Hang

--- src/ui/UI_KhachHang.java
+++ src/ui/UI_KhachHang.java
@@ -7,72 +7,57 @@ package ui;
 * @version: 1.0
 */
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
-import javax.swing.UIManager;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Image;
-import java.awt.Panel;
 import javax.swing.JTextField;
-import javax.swing.border.LineBorder;
 import java.awt.Toolkit;
 import javax.swing.JButton;
-import javax.swing.JTextArea;
-import java.awt.Button;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
+
 import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.awt.event.ActionEvent;
-import javax.swing.JSplitPane;
-import javax.swing.JTabbedPane;
 import javax.swing.border.TitledBorder;
-import java.awt.Checkbox;
-import java.awt.Choice;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 import javax.swing.JScrollPane;
 import java.awt.SystemColor;
-import com.toedter.calendar.JDayChooser;
 
 import connect.ConnectDB;
 import dao.Dao_KhachHang;
 import entity.KhachHang;
 
-import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.JDateChooser;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.border.MatteBorder;
 
 public class UI_KhachHang extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtmaKh;
 	private JTextField txttenKh;
 	private JTextField txtdiaChi;
 	private JTextField txtEmail;
+	private JTextField txtchuThich;
 	private JTextField txtsoDt;
 	private JTable table;
 	private JButton btnThem,btnXoa,btncapNhat,btnlamMoi,btntimKiem;
@@ -81,7 +66,6 @@ public class UI_KhachHang extends JFrame {
 	private JDateChooser dcngaySinh;
 	private Dao_KhachHang dao_kh;
 	private JTextField txtCmnd;
-	private JTextField txtghiChu;
 	/**
 	 * Launch the application.
 	 */
@@ -101,6 +85,7 @@ public class UI_KhachHang extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public UI_KhachHang() {
 		
 		try {
@@ -131,6 +116,8 @@ public class UI_KhachHang extends JFrame {
 		lbltimKiem.setBounds(10, 14, 237, 26);
 		panelTimkiem.add(lbltimKiem);
 
+	
+
 		btntimKiem = new JButton("Tìm Kiếm");
 		btntimKiem.addMouseListener(new MouseAdapter() {
 			@Override
@@ -148,7 +135,7 @@ public class UI_KhachHang extends JFrame {
 							loadKH();
 						}catch(SQLException e1) {
 							e1.printStackTrace();
-					 }
+						}
 				}
 			}
 		});
@@ -245,18 +232,6 @@ public class UI_KhachHang extends JFrame {
 		panelthongTin.add(lblNewLabel_5);
 
 		
-<<<<<<< .mine
-||||||| .r86
-		
-
-
-		JLabel lblchuThich = new JLabel("Chú Thích");
-		lblchuThich.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblchuThich.setBounds(378, 114, 57, 19);
-		panelthongTin.add(lblchuThich);
-
-		
-=======
 		
 
 
@@ -266,7 +241,6 @@ public class UI_KhachHang extends JFrame {
 		panelthongTin.add(lblchuThich);
 
 		
->>>>>>> .r101
 		txtmaKh = new JTextField();
 		txtmaKh.setBounds(137, 26, 276, 20);
 		panelthongTin.add(txtmaKh);
@@ -294,16 +268,6 @@ public class UI_KhachHang extends JFrame {
 		panelthongTin.add(txtEmail);
 		txtEmail.setColumns(10);
 		
-<<<<<<< .mine
-||||||| .r86
-		txtchuThich = new JTextField();
-		txtchuThich.setForeground(Color.BLACK);
-		txtchuThich.setBackground(new Color(255, 255, 255));
-		txtchuThich.setBounds(492, 115, 342, 36);
-		panelthongTin.add(txtchuThich);
-		txtchuThich.setColumns(10);
-		
-=======
 		txtchuThich = new JTextField();
 		txtchuThich.setForeground(Color.BLACK);
 		txtchuThich.setBackground(new Color(255, 255, 255));
@@ -311,7 +275,6 @@ public class UI_KhachHang extends JFrame {
 		panelthongTin.add(txtchuThich);
 		txtchuThich.setColumns(10);
 		
->>>>>>> .r101
 		txtsoDt = new JTextField();
 		txtsoDt.setBounds(137, 84, 276, 20);
 		panelthongTin.add(txtsoDt);
@@ -392,7 +355,7 @@ public class UI_KhachHang extends JFrame {
 				    		String diachi = txtdiaChi.getText();
 				    		String email = txtEmail.getText();
 				    		String sodt = txtsoDt.getText();
-				    		String ghiChu = txtghiChu.getText();
+				    		String ghiChu = txtchuThich.getText();
 				    		KhachHang kh = new KhachHang(makH,tenkH,cmnd,gioiTinh,ngay,diachi,email,sodt,ghiChu);
 				    		dao_kh.update(kh);
 				    		loadKH();
@@ -421,13 +384,12 @@ public class UI_KhachHang extends JFrame {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				txtmaKh.setText("");
-				txtghiChu.setText("");
+				txtchuThich.setText("");
 				txtdiaChi.setText("");
 				txtEmail.setText("");
 				txtsoDt.setText("");
 				txttenKh.setText("");
 				txttimKiem.setText("");
-				txtCmnd.setText("");
 				dcngaySinh.setCalendar(null);
 				txtmaKh.requestFocus();
 			}
@@ -438,17 +400,7 @@ public class UI_KhachHang extends JFrame {
 		btnlamMoi.setBounds(237, 110, 135, 49);
 		panelchucNang.add(btnlamMoi);
 		
-		dcngaySinh.setDateFormatString("yyyy-MM-dd");
-		
-		JLabel lblghiChu = new JLabel("Ghi Chú");
-		lblghiChu.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblghiChu.setBounds(378, 116, 46, 14);
-		panelthongTin.add(lblghiChu);
-		
-		txtghiChu = new JTextField();
-		txtghiChu.setBounds(492, 113, 342, 20);
-		panelthongTin.add(txtghiChu);
-		txtghiChu.setColumns(10);
+		dcngaySinh.setDateFormatString("yyy-MM-dd");
 	    btnThem = new JButton("Thêm");
 	    btnThem.addMouseListener(new MouseAdapter() {
 	    	@Override
@@ -464,7 +416,7 @@ public class UI_KhachHang extends JFrame {
 	    		String diachi = txtdiaChi.getText();
 	    		String email = txtEmail.getText();
 	    		String sodt = txtsoDt.getText();
-	    		String ghiChu = txtghiChu.getText();
+	    		String ghiChu = txtchuThich.getText();
 	    		KhachHang kh = new KhachHang(makH,tenkH,cmnd,gioiTinh,ngay,diachi,email,sodt,ghiChu);
 	    		dao_kh.themKH(kh);
 	    		JFrame f = new JFrame();
@@ -483,13 +435,6 @@ public class UI_KhachHang extends JFrame {
 		btnThem.setBounds(56, 33, 135, 49);
 		panelchucNang.add(btnThem);
 		
-<<<<<<< .mine
-		
-||||||| .r86
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(23, 156, 1303, 331);
-		contentPane.add(scrollPane);
-=======
 
 		JLabel lblchucNang = new JLabel("Chức Năng");
 		lblchucNang.setBounds(124, 0, 173, 23);
@@ -503,14 +448,10 @@ public class UI_KhachHang extends JFrame {
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 197, 1502, 331);
 		contentPane.add(scrollPane);
->>>>>>> .r101
 		String[] header = {
 				"Mã Khách Hàng", "Tên Khách Hàng","CMND", "Giới Tính", "Ngày Sinh", "Địa Chỉ", "Email", "Số Điện Thoại","Chú Thích"
 		};
 		tableModel = new DefaultTableModel(header,0);
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(23, 156, 1303, 331);
-		contentPane.add(scrollPane);
 		table = new JTable(tableModel);
 		table.addMouseListener(new MouseAdapter() {
 			@Override
@@ -519,12 +460,12 @@ public class UI_KhachHang extends JFrame {
 				txtmaKh.setText(tableModel.getValueAt(i, 0).toString());
 				txttenKh.setText(tableModel.getValueAt(i, 1).toString());
 				txtCmnd.setText(tableModel.getValueAt(i, 2).toString());
-				cbgioiTinh.setSelectedItem(tableModel.getValueAt(i, 3));
+				cbgioiTinh.setSelectedItem(tableModel.getValueAt(i, 3).toString());
 				dcngaySinh.setDate(Date.valueOf(LocalDate.parse(tableModel.getValueAt(i, 4).toString())));
 				txtdiaChi.setText(tableModel.getValueAt(i, 5).toString());
 				txtEmail.setText(tableModel.getValueAt(i, 6).toString());
 				txtsoDt.setText(tableModel.getValueAt(i, 7).toString());
-				txtghiChu.setText(tableModel.getValueAt(i, 8).toString());
+				txtchuThich.setText(tableModel.getValueAt(i, 8).toString());
 			}
 		});
 		
@@ -575,7 +516,7 @@ public class UI_KhachHang extends JFrame {
 	//Đếm số KhachHang
 	private void timKhachHang() throws SQLException {
 		dao_kh = new Dao_KhachHang();
-		tableModel = dao_kh.timKiem(txttimKiem.getText());
+		tableModel = dao_kh.timKiem("%"+txttimKiem.getText()+"%");
 		table.setModel(tableModel);
 		
 	}
@@ -587,7 +528,7 @@ public class UI_KhachHang extends JFrame {
 		String diachi = txtdiaChi.getText().trim();
 		String email = txtEmail.getText().trim();
 		String sodt = txtsoDt.getText().trim();
-		String ghichu = txtghiChu.getText().trim();
+		String ghichu = txtchuThich.getText().trim();
 		if(!(makh.length()>0 && makh.matches("[K][H]\\d{3}"))) {
 			JOptionPane.showMessageDialog(null, "Mã khách hàng không để trống và phải bắt đầu từ KH và theo sau là 3 số"  );
 			return false;
