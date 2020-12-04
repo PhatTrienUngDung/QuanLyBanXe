dbd066a7cd54f82bd607cf39601d9d746a0c7b96
hungnhat130420 <57965451+hungnhat130420@users.noreply.github.com>
2020-12-04 09:31:06 +0700

    log

--- src/ui/Login.java
+++ src/ui/Login.java
@@ -11,11 +11,14 @@ package ui;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import AppPackage.AnimationClass;
+//import AppPackage.AnimationClass;
 import connect.ConnectDB;
 import dao.Dao_TaiKhoan;
 import entity.TaiKhoan;
@@ -28,6 +31,9 @@ import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.awt.Font;
 import javax.swing.JTextField;
@@ -50,7 +56,7 @@ public class Login extends JFrame {
 	private JPanel contentPane; 
 	private JLabel lblAnh;
 	private JLabel lbltieuDe;
-	private JLabel lbluser;
+	private JLabel lbluser,lblbackground;
 	private JLabel lblpass;
 	private JPanel paneluser;
 	private JPanel panelpass;
@@ -59,6 +65,8 @@ public class Login extends JFrame {
 	public static JTextField txtuser;
 	private JPasswordField txtpass;
 	private JLabel lblxoauser,lblxoapass;
+	AnimationClass AC = new AnimationClass();
+	private JLabel lblanh1,lblanh2;
 	/**
 	 * Launch the application.
 	 */
@@ -90,20 +98,43 @@ public class Login extends JFrame {
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		Dao_TaiKhoan dao_tk = new Dao_TaiKhoan();
-		setBounds(-10, 0, 1430, 660);
+		setBounds(-10, 0, 1430, 750);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JPanel panelRight = new JPanel();
-		panelRight.setBounds(10, 11, 843, 599);
+		panelRight.setBounds(3, 67, 840, 559);
 		contentPane.add(panelRight);
 		panelRight.setLayout(null);
 		
+		lblanh1 = new JLabel("");
+		//lblanh1.setIcon(new ImageIcon(Login.class.getResource("/img1/inventory-management-system.png")));
+		lblanh1.setBounds(0, 11, 840, 550);
+		ImageIcon icon1 = new ImageIcon("src/img1/inventory-management-system.png");
+		Image img1 = icon1.getImage();
+		Image imgScale1 = img1.getScaledInstance(lblanh1.getWidth(), lblanh1.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon scaledIcon1 = new ImageIcon(imgScale1);
+		lblanh1.setIcon(scaledIcon1);
+		
+		panelRight.add(lblanh1);
+		
+		lblanh2 = new JLabel("");
+		//lblanh2.setIcon(new ImageIcon(Login.class.getResource("/img1/images.jpg")));
+		lblanh2.setBounds(840, 11, 840, 550);
+		ImageIcon icon2 = new ImageIcon("src/img1/seed_fundraising_pitch_deck_business_plan_animation_ppt_download_315300.jpg");
+		Image img2 = icon2.getImage();
+		Image imgScale2 = img2.getScaledInstance(lblanh2.getWidth(), lblanh2.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon scaledIcon2 = new ImageIcon(imgScale2);
+		lblanh2.setIcon(scaledIcon2);
+		//lblanh2.setIcon(new ImageIcon(Login.class.getResource("/img1/seed_fundraising_pitch_deck_business_plan_animation_ppt_download_315300.jpg")));
+		
+		panelRight.add(lblanh2);
+		
 		JPanel panelLeft = new JPanel();
 		panelLeft.setBackground(new Color(255, 255, 255));
-		panelLeft.setBounds(853, 11, 507, 599);
+		panelLeft.setBounds(853, 28, 507, 652);
 		contentPane.add(panelLeft);
 		panelLeft.setLayout(null);
 		
@@ -166,7 +197,7 @@ public class Login extends JFrame {
 		panelLeft.add(txtuser);
 		txtuser.setColumns(10);
 		
-		JButton btndangNhap = new JButton("Đăng Nhập");
+		JButton btndangNhap = new JButton("Login");
 		btndangNhap.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -178,7 +209,9 @@ public class Login extends JFrame {
 					//txtuser.setText("");
 					//txtpass.setText("");
 					//close();
+
 					Menu m = null;
+				
 					try {
 						m = new Menu();
 						m.setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -186,6 +219,7 @@ public class Login extends JFrame {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
+
 					m.setVisible(true);
 					close();
 				}
@@ -194,8 +228,8 @@ public class Login extends JFrame {
 				}
 			}
 		});
-		btndangNhap.setForeground(new Color(0, 0, 0));
-		btndangNhap.setBackground(new Color(153, 102, 51));
+		btndangNhap.setForeground(SystemColor.inactiveCaptionBorder);
+		btndangNhap.setBackground(new Color(0, 153, 255));
 		btndangNhap.setFont(new Font("Tahoma", Font.BOLD, 25));
 		btndangNhap.setBounds(143, 524, 202, 49);
 		panelLeft.add(btndangNhap);
@@ -213,6 +247,8 @@ public class Login extends JFrame {
 						//txtuser.setText("");
 						//txtpass.setText("");
 						//close();
+
+
 						Menu m = null;
 						try {
 							m = new Menu();
@@ -221,6 +257,7 @@ public class Login extends JFrame {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
+
 						m.setVisible(true);
 						close();
 					}
@@ -274,10 +311,79 @@ public class Login extends JFrame {
 		lblxoapass.setBounds(359, 465, 17, 16);
 		lblxoapass.setVisible(false);
 		panelLeft.add(lblxoapass);
+		
+		
+//		BufferedImage img = null;
+//		try {
+//		    img = ImageIO.read(new File("/img1/Banner_CB300R-1024x453.jpg"));
+//		} catch (IOException e) {
+//		    e.printStackTrace();
+//		}
+//		
+		
+		lblbackground = new JLabel("");
+		lblbackground.setBounds(0, 0, 1370, 711);
+		ImageIcon icon = new ImageIcon("src/img1/light-background-wallpaper-preview.jpg");
+		Image img = icon.getImage();
+		Image imgScale = img.getScaledInstance(lblbackground.getWidth(), lblbackground.getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon scaledIcon = new ImageIcon(imgScale);
+		lblbackground.setIcon(scaledIcon);
+		//lblbackground.setIcon(new ImageIcon(Login.class.getResource("/img1/Banner_CB300R-1024x453.jpg")));
+		contentPane.add(lblbackground);
+		slideshow();
 	}
 	private void close() {
 		WindowEvent winClosing = new WindowEvent(this,WindowEvent.WINDOW_CLOSING);
 		Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(winClosing);
+	}
+	public void slideshow() {
+		new Thread() {
+			int count;
+			public void run() {
+				try {
+					while(true)
+					{
+						switch(count)
+						{
+							case 0:
+								
+								Thread.sleep(3000);
+								AC.jLabelXLeft(0, -840, 5, 5, lblanh1);
+								AC.jLabelXLeft(840, 0, 5, 5, lblanh2);
+						
+								count = 1;
+								break;
+							case 1:
+								
+								Thread.sleep(3000);
+								AC.jLabelXRight(-840, 0, 5, 5, lblanh1);
+								AC.jLabelXRight(0,840, 5, 5, lblanh2);
+								
+								count = 2;
+								break;	
+							case 2:
+								
+								Thread.sleep(3000);
+								AC.jLabelXLeft(0, -840, 5, 5, lblanh1);
+								AC.jLabelXLeft(840, 0, 5, 5, lblanh2);
+								
+								count = 3;
+								break;		
+							case 3:
+								
+								Thread.sleep(3000);
+								AC.jLabelXRight(-840, 0, 5, 5, lblanh1);
+								AC.jLabelXRight(0,840, 5, 5, lblanh2);
+								
+								count = 0;
+								break;			
+						}
+					}
+				}catch(Exception e) {
+					
+				}
+			}
+		}.start();
 	}
 }
 
