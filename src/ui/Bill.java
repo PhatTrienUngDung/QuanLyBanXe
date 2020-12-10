9f6c9aa7d1d6349d731c449dbbbdabc166a6a5fe
NguyenThanhHoai15 <73808149+NguyenThanhHoai15@users.noreply.github.com>
2020-12-10 17:24:21 +0700

    commit

---
+++ src/ui/Bill.java
@@ -0,0 +1,49 @@
+package ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+
+public class Bill extends JFrame {
+
+	private JPanel contentPane;
+	public static JTextArea textArea;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Bill frame = new Bill();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Bill() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		setLocationRelativeTo(null);
+		textArea = new JTextArea();
+		textArea.setBounds(5, 5, 426, 248);
+		textArea.append("Cửa Hàng Xe Thành Nàm");
+		textArea.append("\n Số tiền của khách hàng là "+ UI_ThanhToan.txtCusMoney.getText());
+		contentPane.add(textArea);
+	}
+
+}
